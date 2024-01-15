46aab69263610b3227ad67ccffb4a1389e5d198a
ECdub27 <ewiegand51@gmail.com>
2024-01-15 12:04:11 -0600

    nav-bar

---
+++ src/Components/NavBar/navBar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState }  from "react";
+import  Button  from '@mui/material/Button';
+import  Menu from '@mui/material/Menu';
+import  MenuItem  from '@mui/material/MenuItem';
+
+function NavBar(){
+const [anchorElement, setAnchorElement] = useState(null);
+const open  = Boolean(anchorElement);
+const handleClick = (event) =>{
+    setAnchorElement(event.currentTarget);
+};
+const handleClose = (event) =>{
+    setAnchorElement(null);
+}
+
+
+
+
+    return (
+        <div>
+       <Button  id='nav-bar-button'
+       aria-controls= {open ? 'nav-bar-button' :undefined}
+       aria-haspopup = 'true'
+       aria-expanded = {open ? 'true': undefined}
+       onClick= {handleClick}>
+        Explore Website
+       </Button>
+       <Menu id='nav-bar-menu'
+       anchorElement={anchorElement}
+       open={open}
+       onClose ={handleClose}
+       menuListProps = {{'aria-labelledby':'nav-bar-button' }}>
+        <MenuItem className='nav-bar-i1' onClick={handleClose}>Culture</MenuItem>
+        <MenuItem className='nav-bar-i2' onClick={handleClose}>Sports</MenuItem>
+        <MenuItem className='nav-bar-i3' onClick={handleClose}>Politics</MenuItem>
+        <MenuItem className='nav-bar-i1'onClick={handleClose}>Science</MenuItem>
+        <MenuItem className='nav-bar-i2'onClick={handleClose}>Business</MenuItem>
+        <MenuItem className='nav-bar-i3'onClick={handleClose}>Technology</MenuItem>
+       </Menu>
+        </div>
+
+);
+}
+
+
+
+export default NavBar
