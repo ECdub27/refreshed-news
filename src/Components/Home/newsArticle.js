82a5528f5a2e0a50a50b8f71db689dae2fed8c17
ECdub27 <ewiegand51@gmail.com>
2024-01-17 12:55:15 -0600

    testing

--- src/Components/Home/newsArticle.js
+++ src/Components/Home/newsArticle.js
@@ -1,10 +1,27 @@
 import {useState, useEffect} from 'react';
+import { getNewsArticlesStatus,getNewsArticleSuccess,getNewsArticleFailure,fetchNewsArticles } from '../../store/newsArticleSlice';
+import {useDispatch,useSelector} from 'react-redux';
+import { selectArticles } from '../../store/newsArticleSlice';
 
- 
+
+
 const NewsArticle = () =>{ 
+const dispatch = useDispatch();
+const articles = useSelector(selectArticles);
+
+useEffect(() =>{
+dispatch(fetchNewsArticles);
+},[dispatch]
+
+);
 
 
+return (
+    <div>
+        
+    </div>
 
+);
 
 
 };
