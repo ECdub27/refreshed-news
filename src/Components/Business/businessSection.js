dbd8a8c6b582b156155ae1918512de584a03939d
ECdub27 <ewiegand51@gmail.com>
2024-02-02 15:32:04 -0600

    throw into as cards

--- src/Components/Business/businessSection.js
+++ src/Components/Business/businessSection.js
@@ -1,27 +1,58 @@
 
 import {fetchBusinessNews, getBusinessNewsStatus} from '../../store/businessSectionSlice';
 import {useSelector, useDispatch} from 'react-redux';
-import { selectBusinessNews } from '../../store/businessSectionSlice';
-import { useEffect } from 'react';
+ import { selectBusinessNews } from '../../store/businessSectionSlice';
+import { useEffect,useState } from 'react';
+import { Troubleshoot } from '@mui/icons-material';
 
 
 
 const BusinessSection = () =>{
 
 const dispatch = useDispatch();
-const selectedBusinessArticles = useSelector(selectBusinessNews);
-const businessNewsStatus = useSelector(getBusinessNewsStatus);
+ const businessArticles = useSelector((state) => state.businessNews);
+ const {articles,status, error, isLoading} = businessArticles
+ const businessNewsStatus = useSelector(getBusinessNewsStatus);
+
+
+
 
 useEffect(() =>{
-dispatch(fetchBusinessNews());
-},[dispatch]);
+    let isMounted = true
+if(status === 'idle'){
+    dispatch(fetchBusinessNews())
+}
+return () => {
+    isMounted = false
+}
+},[status, dispatch])
 // map out return promise also send the right headers
+let bodyContent
+if (status === 'loading'){
+ bodyContent = <div className='loader'></div>
+} else if (status === 'successful'){
+let sortedArticles;
+
+ sortedArticles = articles.slice().sort((a,b) => b.id -a.id)
+
+
+    bodyContent = sortedArticles.map((article) => (
+        <div className='card' key= {article.id}>
+<ul key={article.id}>
+                <li key={article.id}>{article.name} <p>{article.description}</p></li>
+                
+
+            </ul>
+        </div>
+    ))
+} else {
+    bodyContent = <div>{error}</div>
+}
+
 return (
     <div>
-       <div>{selectedBusinessArticles} </div>
-
-<button onClick={() => dispatch(fetchBusinessNews())}>fetch news</button>
-    <p>{businessNewsStatus === 'loading' && <div>fetching News...</div>}</p>
+       <h2>Business News</h2>
+       {bodyContent}
     </div>
    
 )
