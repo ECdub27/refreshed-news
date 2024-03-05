f4b9d115751c5bf10aa63f0ed18a0f40dbcaaed6
ECdub27 <ewiegand51@gmail.com>
2024-03-05 07:23:14 -0600

    fix up last two sections

--- src/App.js
+++ src/App.js
@@ -83,9 +83,54 @@ const {data, error, isLoading } = useGetPokemonByNameQuery('eevee')
     <main>
   <div className='top-headlines-div'>
     <HeroSlide />
+    <NewsArticle/>
     </div>
     </main>
-      
+    <div className="Business.Section.div">
+<div>
+<hr className='divider-3'/>
+   <h3>Whos that pokemon!!</h3>
+   {error ? (
+     <>Oh no, there was an error</>
+   ) : isLoading ? (
+     <>Loading...</>
+   ) : data ? (
+     <>
+     <p>{data.species.name}</p> 
+     <img src={data.sprites.front_shiny} alt={data.species.name}/>  
+     </>
+   ) : null
+ }
+   
+    </div>
+    
+</div>
+
+
+<hr className='divider-3'/>
+
+<div className='Technology.Section.div'>
+ <Container>
+   <TechnologySection />
+   </Container>
+</div>
+
+<div className='Politics.Section.div'>
+<hr/>
+
+ <PoliticsSection />
+ 
+</div>
+
+
+<hr className='divider-4'/>
+
+
+ <div className='footer-container'>
+   Refreshed news
+  <Footer />
+ </div>
+ 
     
 
 </ThemeProvider>
